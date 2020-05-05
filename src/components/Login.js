df671a6e46ed1729afffe914e69853f1b7e772ea
heydonhatcher <heyhatch@gmail.com>
2020-05-05 14:23:18 -0500

    working on design

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,70 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import { GOOGLE_IDENTITY_CLIENT_ID } from "../constants/protected";
 
-export default ({ loginSuccess, loginFailure }) => (
-  <GoogleLogin
-    clientId={GOOGLE_IDENTITY_CLIENT_ID}
-    buttonText="Login"
-    onSuccess={loginSuccess}
-    onFailure={loginFailure}
-    cookiePolicy={"single_host_origin"}
-  />
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: theme.palette.grey[100],
+  },
+  image: {
+    backgroundImage: "url(" + require("../logojpg.jpg") + ")",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+export default ({ loginSuccess, loginFailure }) => {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in!
+          </Typography>
+          <GoogleLogin
+            clientId={GOOGLE_IDENTITY_CLIENT_ID}
+            buttonText="Log into Movie Roulette"
+            width="240"
+            height="50"
+            longtitle="true"
+            theme="dark"
+            onSuccess={loginSuccess}
+            onFailure={loginFailure}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
--- src/components/MovieSearchInput.js
+++ src/components/MovieSearchInput.js
@@ -1,20 +1,89 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import TheatersIcon from "@material-ui/icons/Theaters";
+import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-export default ({ title, onTitleChange, onTitleSubmit }) => (
-  <form>
-    <TextField
-      value={title}
-      onChange={event => onTitleChange(event.target.value)}
-      label="Standard"
-    />
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={() => onTitleSubmit(title)}
-    >
-      Submit
-    </Button>
-  </form>
-);
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+}));
+
+export default ({ title, onTitleChange, onTitleSubmit }) => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <TheatersIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Welcome to Movie Roulette
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Search for a movie:
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Tired of wasting too much time deciding on a movie? Let's discover
+              other movies peppered with the writers, directors, and actors you
+              know and love.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <TextField
+                    value={title}
+                    onChange={(event) => onTitleChange(event.target.value)}
+                    label="Search"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => onTitleSubmit(title)}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+};
