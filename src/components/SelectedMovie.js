c71d92d150de45f183cf52c01c5382a6abe01fc7
heydonhatcher <heyhatch@gmail.com>
2020-05-12 10:47:58 -0500

    got selected movie centered

--- src/components/SelectedMovie.js
+++ src/components/SelectedMovie.js
@@ -1,30 +1,38 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import { moviePropType } from "../constants/propTypes";
 import Movie from "./Movie";
 import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 400,
-    padding: 15,
-    position: "relative",
+    minWidth: 300,
+    padding: 25,
+    margin: "auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "lightBlue",
   },
-  titleOne: {
-    fontSize: 18,
-    color: "gray",
-  },
+  // titleOne: {
+  //   fontSize: 18,
+  //   color: "gray",
+  // },
 });
 
 const SelectedMovie = ({ selectedMovie }) => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Typography className={classes.titleOne}>Selected Movie:</Typography>
-      <Movie movie={selectedMovie} />
-    </Paper>
+    <React.Fragment>
+      <Card className={classes.root}>
+        {/* <Typography variant="h5" className={classes.titleOne}>
+          Selected Movie:
+        </Typography> */}
+        <Movie movie={selectedMovie} />
+      </Card>
+    </React.Fragment>
   );
 };
 
